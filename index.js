86077e52895f7afd841f9e7cc3663e3020348d85
rajaguru24 <rajaguru246@gmail.com>
2024-07-16 11:57:33 +0530

    completed

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ import router from './Routes/Routes.js';
 
 const app = express();
 app.use(bodyParser.json());
+app.get('/',(req,res)=>
+    {
+       res.send("Express App is running");
+    })
 
-app.use('/api', router);
-const PORT = 4010;
+app.use('/', router);
+const PORT = 4000;
 app.listen(PORT, () => console.log(`Started server hallBooking on port ${PORT}`));
